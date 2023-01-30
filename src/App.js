af4ac4d30968bee553dbc950dfe6ed7dc09cb948
radhika sinkar <radhikasinkar8@gmail.com>
2023-01-30 18:11:59 +0530

    password-generator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import PasswordApp from './PasswordApp';
 import Pass from './Pass';
 
 function App() {
   return (
     <div>
-      {/* <PasswordApp /> */}
       <Pass />
     </div>
   );
